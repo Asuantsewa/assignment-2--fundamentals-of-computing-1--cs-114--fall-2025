15b839b467effd5c2187f4db7110faa22b879508
Jeanelle_Eshun <jeanelle.eshun@gmail.com>
2025-10-11 15:45:04 -0400

    program for vowels and non vowels

--- PrintVowelsAndNonVowels.java
+++ PrintVowelsAndNonVowels.java
@@ -1,4 +1,43 @@
 public class PrintVowelsAndNonVowels {
+import java.util.Scanner;
   public static void main(String[] args) {
-  }
+    Scanner scan = new Scanner(System.in)
+    System.out.println("Enter a string: ");
+    String input = scanner.nextLine();
+
+// Innotialize the counters for each lower case vowel
+   int countA = 0;
+   int countE = 0;
+   int countI = 0;
+   int countO = 0;
+   int countU = 0;
+// Counters for non vowels
+   int nonVowelCount = 0;
+// loop 
+for (int i = 0; i< input.length(); i++){
+  char ch = input.charAt(i); 
+// now we check for the vowel and udate the counter
+    if (ch == 'a'){
+      countA++;
+    }else if (ch == 'e'){
+      countE++;
+    }else if (ch == 'i'){
+      countI++
+    }else if (ch == 'o'){
+      countO++
+    }else if (ch == 'u'){
+      countU++;
+    }else{
+      nonVowelCount++;
+    }
+    }
+// to print results
+   System.out.println("Vowel counts:");
+   System.out.println("a: " + countA);
+   System.out.println("e " + countE );
+   System.out.println("i "+ countI);
+   System.out.println("o "+ countO);
+   System.out.println("u "+ countU);
+   System.out.println("Non vowel characters: "+ nonVowelCount);
+}
 }
