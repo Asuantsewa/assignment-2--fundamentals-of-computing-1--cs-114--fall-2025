f79d6165eb55007eccac78170f447becfb35cafd
Jeanelle_Eshun <jeanelle.eshun@gmail.com>
2025-10-11 14:34:35 -0400

    the switch statement to print nth day

--- TheTwelveDaysOfChristmas.java
+++ TheTwelveDaysOfChristmas.java
@@ -22,6 +22,25 @@ public class TheTwelveDaysOfChristmas {
                      System.out.println("And a patridge in a pear tree.");
                    }
                }
+             System.out.println();
       }
      }
+// to get the suffix of the day(which is the 1st 2nd and 3rd, etc)
+  private static String getDaySuffix(int day){
+     switch (day) {
+          case 1:return "1st";
+          case 2:return "2nd";
+          case 3:return "3rd";
+          case 4:return "4th";
+          case 5:return "5th";
+          case 6:return "6th";
+          case 7:return "7th";
+          case 8:return "8th";
+          case 9:return "9th";
+          case 10:return "10th";
+          case 11:return "11th";
+          case 12: return "12th";
+          default: return "";
+     }
+  }
   }
