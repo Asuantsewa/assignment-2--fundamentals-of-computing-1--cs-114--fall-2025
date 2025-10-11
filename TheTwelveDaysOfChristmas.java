f01f75d57a5529f9c6adf691210300aee429c058
Jeanelle_Eshun <jeanelle.eshun@gmail.com>
2025-10-11 14:14:18 -0400

    new program

--- TheTwelveDaysOfChristmas.java
+++ TheTwelveDaysOfChristmas.java
@@ -1,7 +1,27 @@
 public class TheTwelveDaysOfChristmas {
   public static void main(String[] args) {
+     for (int day = 1; day<= 12; day++) {
+      Sysytem.out.pritln("On the " + getDaySuffix(day) + "day of christmas my ture love gave to me:");
 
       switch (day){
+          case 12: system.out.println("Twelve drummers drumming, ")
+          case 11: System.out.println("Eleven pipers piping,");
+          case 10: System.out.println("Ten lords are leaping,");
+          cae 9: System.out.println("NIne ladies dancing,");
+          case 8: System.out.println("Eight maids a-milking,");
+          case 7: System.out.println("Seven swans a-swimming,");
+          case 6: System.out.println("Six geese a-laying,");
+          case 5: System.out.println("Five golden rings,");
+          case 4: System.out.println("Four calling birds,");
+          case 3: System.out.println("Three french hens,");
+          case 2: System.out.println("Two turtle doves,");
+          case 1: 
+               if (day == 1) {
+                    System.out.println("A partridge in a pear tree.");
+                } else {
+                     System.out.println("And a patridge in a pear tree.");
+                   }
+               }
       }
      }
   }
