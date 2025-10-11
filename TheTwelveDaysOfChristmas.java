6a9f90503ccb22b93c503f11fea986f2750530aa
Jeanelle_Eshun <jeanelle.eshun@gmail.com>
2025-10-11 15:55:50 -0400

    Syntax error with may spacing

--- TheTwelveDaysOfChristmas.java
+++ TheTwelveDaysOfChristmas.java
@@ -28,18 +28,18 @@ public class TheTwelveDaysOfChristmas {
 // to get the suffix of the day(which is the 1st 2nd and 3rd, etc)
   private static String getDaySuffix(int day){
      switch (day) {
-          case 1:return "1st";
-          case 2:return "2nd";
-          case 3:return "3rd";
-          case 4:return "4th";
-          case 5:return "5th";
-          case 6:return "6th";
-          case 7:return "7th";
-          case 8:return "8th";
-          case 9:return "9th";
-          case 10:return "10th";
-          case 11:return "11th";
-          case 12: return "12th";
+          case 1:return "1st ";
+          case 2:return "2nd ";
+          case 3:return "3rd ";
+          case 4:return "4th ";
+          case 5:return "5th ";
+          case 6:return "6th ";
+          case 7:return "7th ";
+          case 8:return "8th ";
+          case 9:return "9th ";
+          case 10:return "10th ";
+          case 11:return "11th ";
+          case 12: return "12th ";
           default: return " ";
      }
    }
