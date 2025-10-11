a3bced46efdaf462340f6ddbbe1f604de5e03012
Jeanelle_Eshun <jeanelle.eshun@gmail.com>
2025-10-11 14:13:36 -0400

    deleted, no need for user input

--- TheTwelveDaysOfChristmas.java
+++ TheTwelveDaysOfChristmas.java
@@ -1,25 +1,6 @@
 public class TheTwelveDaysOfChristmas {
   public static void main(String[] args) {
 
-    for (int day = 1; day<= 12; day++){
-// day with suffix
-       System.out.print("On the " );
-       switch (day) {
-          case 1:
-               System.out.print("1st");
-               break;
-          case 2:
-               System.out.print("2nd");
-               break;
-          case 3: 
-               System.out.print("3rd");
-          default:
-               System.out.print(day + "th");
-               break;
-       }
-       System.out.println(" day of Christmas my true love gavve to me:");
-
-// print gifts
       switch (day){
         
       }
