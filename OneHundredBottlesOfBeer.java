192eaa3ff0d0b522cc479f176741ee978c7472f5
Jeanelle_Eshun <jeanelle.eshun@gmail.com>
2025-10-13 18:18:32 -0400

    added a space for the next line

--- OneHundredBottlesOfBeer.java
+++ OneHundredBottlesOfBeer.java
@@ -19,7 +19,8 @@ public class OneHundredBottlesOfBeer {
   for (int i = 100; i > 100 -verses; i--) {
     System.out.println( i + " bottles of beer on the wall, " + i + " bottles of beer");
     System.out.println("If one of the bottles should happen to fall");
-    System.out.println((i - 1)+ "bottles of beer on the wall");
+    System.out.println((i - 1)+ " bottles of beer on the wall");
+    System.out.println();
     }
     scanner.close();
   }
